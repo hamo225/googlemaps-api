5596f51685142ba96e08d07b9f2f02e377243cd5
Tarek <tarekipad@gmail.com>
2020-09-10 17:32:12 +0200

    get JSON Geocoding outputs from file_get_content git push

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php
 
-file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,
-+Mountain+View,+CA&key=AIzaSyDnd8ieHbfQpER5JFScmz5I6sYOWIXGjnA")
-
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-
-
-</body>
+echo file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,
++Mountain+View,+CA&key=AIzaSyABqYXO_horAEeu42Z_d3K2nDyqeZS4EQw");
